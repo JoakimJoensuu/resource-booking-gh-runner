61dc75fd1945151f83eee94fca7a26bb1fb61d9a
Joakim Joensuu <joakim.joensuu@gmail.com>
2023-07-20 20:01:00 +0300

    Create initial booking-client project

---
+++ booking-client/booking_client/__main__.py
@@ -0,0 +1,4 @@
+from booking_client.cli import entry
+
+if __name__ == "__main__":
+    entry()
---
+++ booking-client/booking_client/cli.py
@@ -0,0 +1,35 @@
+import argparse
+import asyncio
+
+from booking_client.commands import main
+
+# TODO: client book machine_type_1 --wait
+# TODO: client book machine_type_1 --workflow_id 345634
+# TODO: client book machine_type_1 machine_type_1_identifier_123265324
+# TODO: client cancel 123456
+
+
+def entry():
+    # create the top-level parser
+    parser = argparse.ArgumentParser(prog="booking")
+    parser.add_argument("--foo", action="store_true", help="foo help")
+    subparsers = parser.add_subparsers(help="sub-command help", required=True)
+
+    # create the parser for the "a" command
+    parser_a = subparsers.add_parser("a", help="a help")
+    parser_a.add_argument("bar", type=int, help="bar help")
+
+    # create the parser for the "b" command
+    parser_b = subparsers.add_parser("b", help="b help")
+    parser_b.add_argument("--baz", choices="XYZ", help="baz help")
+
+    # parse some argument lists
+    args = parser.parse_args()
+
+    print(args)
+
+    asyncio.run(main())
+    print("ASdD")
+
+    # TODO: To receive notifications https://stackoverflow.com/a/51610341/12820664
+    # TODO: Argcomplete https://stackoverflow.com/a/15289025/12820664
