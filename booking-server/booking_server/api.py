6e887b6f1b3f2440ede842ac48119f9dd9d9cdba
Joakim Joensuu <joakim.joensuu@gmail.com>
2023-07-21 11:24:09 +0300

    Sparate finish and cancel actions in update endpoint

--- booking-server/booking_server/api.py
+++ booking-server/booking_server/api.py
@@ -6,7 +6,7 @@ from typing import List
 from aiohttp import web
 from aiohttp.web_request import Request
 from aiohttp.web_routedef import RouteDef
-from booking_server.booking import BookingStatus, BookingUpdateAction
+from booking_server.booking import BookingStatus
 from booking_server.broker import (
     try_assigning_new_resource,
     try_assigning_to_booking,
@@ -62,7 +62,7 @@ async def new_resource(request: Request):
 
     server_data = await get_server_data(request.app)
 
-    # TODO: Check for existing resources with same identifier
+    # TODO: Check for existing resources with same identifier use HTTP 409 Conflict
 
     server_data["resources"].append(resource)
 
@@ -89,21 +89,7 @@ async def get_bookings(request: Request):
     return web.json_response((await get_server_data(request.app))["bookings"])
 
 
-async def booking_update(request: Request):
-    requested_action = request.match_info["action"]
-
-    if requested_action not in set(
-        action.action_name for action in BookingUpdateAction
-    ):
-        return web.Response(
-            status=HTTPStatus.NOT_FOUND,
-            text=(
-                f"Action {requested_action} not supported. Use one of the"
-                " following:"
-                f" {', '.join([status.action_name for status in BookingUpdateAction])}"
-            ),
-        )
-
+async def booking_finish(request: Request):
     booking_id = int(request.match_info["booking_id"])
     server_data = await get_server_data(request.app)
 
@@ -133,19 +119,80 @@ async def booking_update(request: Request):
             ),
         )
 
-    bookings = server_data["bookings"]
+    if booking["info"]["status"] == BookingStatus.WAITING:
+        return web.Response(
+            status=HTTPStatus.UNPROCESSABLE_ENTITY,
+            text=(
+                f"Booking with id {booking['info']['id']} was still waiting"
+                " for resource to be assigned to it. Did you mean to"
+                " /booking/{booking_id}/cancel the booking?"
+            ),
+        )
 
-    action = BookingUpdateAction[requested_action.upper()]
-    # TODO: Move web.Response construction here
-    return action.updater(booking, bookings)
-    # TODO: Move usage of function for assigning resource to othee booking here
+    freed_resource = booking["used"]
+    if freed_resource is None:
+        raise RuntimeError("Booking didn't have resource even when it should.")
+
+    freed_resource["used_by"] = None
+    booking["info"]["status"] = BookingStatus.FINISHED
+
+    bookings = server_data["bookings"]
+    asyncio.create_task(try_assigning_to_booking(freed_resource, bookings))
+
+    return web.Response()
+
+
+async def booking_cancel(request: Request):
+    booking_id = int(request.match_info["booking_id"])
+    server_data = await get_server_data(request.app)
+
+    try:
+        booking = server_data["id_to_booking"][booking_id]
+    except KeyError:
+        return web.Response(
+            text=f"Booking id {booking_id} doesn't exist.",
+            status=HTTPStatus.NOT_FOUND,
+        )
+
+    if booking["info"]["status"] == BookingStatus.CANCELLED:
+        return web.Response(
+            status=HTTPStatus.UNPROCESSABLE_ENTITY,
+            text=(
+                f"Booking with id {booking['info']['id']} was already"
+                " cancelled."
+            ),
+        )
+
+    if booking["info"]["status"] == BookingStatus.FINISHED:
+        return web.Response(
+            status=HTTPStatus.UNPROCESSABLE_ENTITY,
+            text=(
+                f"Booking with id {booking['info']['id']} was already"
+                " finished."
+            ),
+        )
+
+    if booking["info"]["status"] == BookingStatus.ON:
+        return web.Response(
+            status=HTTPStatus.UNPROCESSABLE_ENTITY,
+            text=(
+                f"Booking with id {booking['info']['id']} had already resource"
+                " assigned to it. Did you mean to"
+                " /booking/{booking_id}/finish the booking?"
+            ),
+        )
+
+    booking["info"]["status"] = BookingStatus.CANCELLED
+
+    return web.Response()
 
 # TODO: Add /booking/extend
 routes: List[RouteDef] = [
     web.get("/booking/all", get_bookings),
     web.get("/bookings", get_bookings),
     web.post("/booking", new_booking),
-    web.post("/booking/{booking_id}/{action}", booking_update),
+    web.post("/booking/{booking_id}/finish", booking_finish),
+    web.post("/booking/{booking_id}/cancel", booking_cancel),
     web.get("/booking/before/{booking_id}", unimplemented),
     web.get("/booking/{booking_id}", get_booking),
     web.post("/resource", new_resource),
--- booking-server/booking_server/booking.py
+++ booking-server/booking_server/booking.py
@@ -1,13 +1,6 @@
 from __future__ import annotations
 
-import asyncio
 from enum import Enum
-from http import HTTPStatus
-from typing import Callable, List
-
-from aiohttp import web
-from booking_server.broker import try_assigning_to_booking
-from booking_server.types import Booking
 
 
 class BookingStatus(str, Enum):
@@ -15,59 +8,3 @@ class BookingStatus(str, Enum):
     FINISHED = "FINISHED"
     WAITING = "WAITING"
     ON = "ON"
-
-
-def cancel_booking(booking: Booking, _: List[Booking]):
-    if booking["info"]["status"] == BookingStatus.ON:
-        return web.Response(
-            status=HTTPStatus.UNPROCESSABLE_ENTITY,
-            text=(
-                f"Booking with id {booking['info']['id']} had already resource"
-                " assigned to it. Did you mean to"
-                " /booking/{booking_id}/finish the booking?"
-            ),
-        )
-
-    booking["info"]["status"] = BookingStatus.CANCELLED
-
-    return web.Response()
-
-
-def finish_booking(booking: Booking, bookings: List[Booking]):
-    if booking["info"]["status"] == BookingStatus.WAITING:
-        return web.Response(
-            status=HTTPStatus.UNPROCESSABLE_ENTITY,
-            text=(
-                f"Booking with id {booking['info']['id']} was still waiting"
-                " for resource to be assigned to it. Did you mean to"
-                " /booking/{booking_id}/cancel the booking?"
-            ),
-        )
-
-    freed_resource = booking["used"]
-    if freed_resource is None:
-        raise RuntimeError("Booking didn't have resource even when it should.")
-
-    freed_resource["used_by"] = None
-    booking["info"]["status"] = BookingStatus.FINISHED
-
-    # TODO: Move to api.py
-    asyncio.create_task(try_assigning_to_booking(freed_resource, bookings))
-
-    return web.Response()
-
-
-class BookingUpdateAction(Enum):
-    def __init__(
-        self,
-        action_name: str,
-        updater: Callable[[Booking, List[Booking]], web.Response],
-    ):
-        self.action_name: str = action_name
-
-        self.updater: Callable[[Booking, List[Booking]], web.Response] = (
-            updater
-        )
-
-    CANCEL = ("cancel", cancel_booking)
-    FINISH = ("finish", finish_booking)
